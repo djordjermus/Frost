b38c25454db0c7b78e9e25483c06582ace4e61be
djordjermus <djordje.rmus2000@google.com>
2023-08-19 11:16:49 +0200

    introduce random api

---
+++ Frost.Api/_src/random.api.cpp
@@ -0,0 +1,41 @@
+#include "../random.api.hpp"
+#include <math.h>
+#include <iostream>
+extern "C"
+{
+	static constexpr u64 x = 12;
+	static constexpr u64 y = 25;
+	static constexpr u64 z = 27;
+	static constexpr u64 m = 2685821657736338717ULL;
+
+	FROST_API u64 _stdcall random_generate(u64* p_seed)
+	{
+		u64 ret = *p_seed;
+		ret ^= ret << x;
+		ret ^= ret >> y;
+		ret ^= ret << z;
+		*p_seed = ret * m;
+
+		return ret;
+	}
+	FROST_API u64 _stdcall random_generate_range(u64* p_seed, u64 min, u64 range)
+	{
+		u64 ret = *p_seed;
+		ret ^= ret << x;
+		ret ^= ret >> y;
+		ret ^= ret << z;
+		*p_seed = ret * m;
+
+		return min + ret % range;
+	}
+	FROST_API f64 _stdcall random_generate_range_double(u64* p_seed, f64 min, f64 range)
+	{
+		u64 ret = *p_seed;
+		ret ^= ret << x;
+		ret ^= ret >> y;
+		ret ^= ret << z;
+		*p_seed = ret * m;
+		std::cout << "MIN: " << min << ", RANGE: " << range << ", RET: " << fmod(ret * 0.0000000000000001, 1.0f) << '\n';
+		return min + fmod(ret * 0.0000000000000001, 1.0f) * range;
+	}
+}
