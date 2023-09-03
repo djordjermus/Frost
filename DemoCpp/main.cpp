fb9aaa5795d25ceaada78b37af75fe2a68acced3
djordjermus <djordje.rmus2000@google.com>
2023-09-03 16:53:48 +0200

    Implement event system i Frost

--- DemoCpp/main.cpp
+++ DemoCpp/main.cpp
@@ -1,4 +1,19 @@
+#include "Frost/event_system.hpp"
+#include <iostream>
+class TextClass
+{
+public:
+	const char* text = "Hello World!";
+	static void print(TextClass* text_class)
+	{
+		std::cout << text_class->text << '\n';
+	}
+};
 int main()
 {
-	
+	frost::event_system::subscribe(1, TextClass::print);
+	frost::event_system::subscribe(3, TextClass::print);
+	TextClass x = TextClass();
+	x.text = ":)";
+	frost::event_system::emit(1, &x);
 }
