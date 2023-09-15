15ca1af77a89dc040b5cbbcd96efc51f28a99ff7
djordjermus <djordje.rmus2000@google.com>
2023-09-15 18:45:51 +0200

    Add pimpl pattern to frost

--- DemoCpp/main.cpp
+++ DemoCpp/main.cpp
@@ -1,30 +1,27 @@
 #include "Frost/event_system.hpp"
 #include "Frost/log.hpp"
+#include "Frost/pimpl.hpp"
 #include <iostream>
-class TextClass
+class base final : public frost::pimpl_crtp<base>
 {
 public:
-	const char* text = "Hello World!";
-	static void print(TextClass* text_class)
-	{
-		std::cout << text_class->text << '\n';
-	}
+	base();
+	base(u64 value);
+	base(base& move);
+	base(base&& move);
+
 };
-void PrintLogEvent(frost::log::event_data* e)
-{
-	std::wcout << e->message_template << L'\n';
-	std::wcout << e->message << L'\n';
 
-	std::wcout << L"PARAMS:\n";
-	for (auto& param : e->parameters)
-		std::wcout << L'\t' << param << L'\n';
-
-	std::wcout << e->timestamp << L'\n';
-	std::wcout << e->thread_id << L'\n';
-	std::wcout << (int)e->level << L'\n';
-}
 int main()
 {
-	frost::event_system::subscribe(1, PrintLogEvent);
-	frost::log::critical(1, L"{0}, {1}, {2}, {3}, {", {L"", L"abc", L"123"});
+	base a(5);
+	base b(10);
+	auto x = b.get_pimpl();
+	b.swap_pimpl(a);
 }
+
+
+base::base() : frost::pimpl_crtp<base>() {}
+base::base(u64 value) : frost::pimpl_crtp<base>((void*)value) {}
+base::base(base& move) : frost::pimpl_crtp<base>(move) {}
+base::base(base&& move) : frost::pimpl_crtp<base>(move) {}
