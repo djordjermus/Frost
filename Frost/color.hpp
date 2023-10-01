9ad96cffa94abf239a4b9676766382602168006a
djordjermus <djordje.rmus2000@google.com>
2023-10-01 11:20:08 +0200

    Add abgr support to color api

--- Frost/color.hpp
+++ Frost/color.hpp
@@ -29,7 +29,10 @@ namespace frost
 		{
 		public:
 			STATIC_CLASS(api);
-			static void FROST_API color_rgba8_to_rgba32(const u32 input, f32* output);
+			static void FROST_API rgba8_to_abgr8(const u32 input, u32* output);
+			static void FROST_API abgr8_to_rgba8(const u32 input, u32* output);
+
+			static void FROST_API rgba8_to_rgba32(const u32 input, f32* output);
 		};
 	};
 
@@ -49,15 +52,15 @@ namespace frost
 		{
 		public:
 			STATIC_CLASS(api);
-			static void FROST_API color_rgba32_to_rgba8(const f32* input, u32* output);
+			static void FROST_API rgba32_to_rgba8(const f32* input, u32* output);
 
-			static void FROST_API color_rgba32_to_hsva(const f32* input, f32* output);
-			static void FROST_API color_rgba32_to_hsla(const f32* input, f32* output);
-			static void FROST_API color_rgba32_to_cmyk(const f32* input, f32* output);
+			static void FROST_API rgba32_to_hsva(const f32* input, f32* output);
+			static void FROST_API rgba32_to_hsla(const f32* input, f32* output);
+			static void FROST_API rgba32_to_cmyk(const f32* input, f32* output);
 
-			static void FROST_API color_hsva_to_rgba32(const f32* input, f32* output);
-			static void FROST_API color_hsla_to_rgba32(const f32* input, f32* output);
-			static void FROST_API color_cmyk_to_rgba32(const f32* input, f32* output);
+			static void FROST_API hsva_to_rgba32(const f32* input, f32* output);
+			static void FROST_API hsla_to_rgba32(const f32* input, f32* output);
+			static void FROST_API cmyk_to_rgba32(const f32* input, f32* output);
 		};
 	};
 
