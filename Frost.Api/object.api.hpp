26bda6a028ee14d285af2c7d4f1b8572e4fbac9a
djordjermus <djordje.rmus2000@google.com>
2023-10-15 09:43:33 +0200

    do not dll export inlined function of object api

--- Frost.Api/object.api.hpp
+++ Frost.Api/object.api.hpp
@@ -3,7 +3,7 @@
 #pragma once
 namespace frost::api
 {
-	class FROST_API object
+	class object
 	{
 	private:
 		u64 _reference_count = 0;
@@ -15,8 +15,8 @@ namespace frost::api
 		inline void acquire_reference() { return acquire_reference(this); }
 		inline void release_reference() { return release_reference(this); }
 
-		static u64 _stdcall get_reference_count(object* target);
-		static void _stdcall acquire_reference(object* target);
-		static void _stdcall release_reference(object* target);
+		static FROST_API u64 _stdcall get_reference_count(object* target);
+		static FROST_API void _stdcall acquire_reference(object* target);
+		static FROST_API void _stdcall release_reference(object* target);
 	};
 }
