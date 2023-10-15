01551b17ead8cd216d6f0665f82e423a7d277c3b
djordjermus <djordje.rmus2000@google.com>
2023-10-15 13:28:07 +0200

    temporarily remove sync objects implementation from Frost C++

--- DemoCpp/main.cpp
+++ DemoCpp/main.cpp
@@ -1,21 +1,39 @@
+#include "Frost.Api/sync_object.api.hpp"
 #include "Frost.Api/object.api.hpp"
 #include "Frost.Api/ref.hpp"
 #include <iostream>
-class my_object : public frost::api::object
-{
-	u64 _value;
-protected:
-	my_object(u64 value);
-	~my_object();
-public:
-	static my_object* create(u64 value);
-};
+#include <vector>
+void test_sync_object();
 int main()
 {
-	ref<my_object> obj = my_object::create(5);
-	ref<my_object> obj2 = obj;
-	obj = nullptr;
 }
-my_object::my_object(u64 value) : _value(value) {}
-my_object::~my_object() { std::cout << "delete my_object " << _value << '\n'; }
-my_object* my_object::create(u64 value) { return new my_object(value); }
+
+void test_sync_object()
+{
+	
+
+	ref<frost::api::sync_object> mx = frost::api::sync_object::create_mutex(false);
+	ref<frost::api::sync_object> sf = frost::api::sync_object::create_semaphore(2, 2);
+	sf = frost::api::sync_object::create_mutex(false);
+	bool b;
+	i32 i;
+	b = mx->lock();		// TRUE
+	b = sf->lock();		// TRUE
+	b = sf->lock();		// TRUE
+	b = sf->try_lock();	// FALSE
+	b = mx->unlock();	// TRUE
+	b = mx->unlock();	// FALSE
+	b = sf->unlock();	// TRUE
+	b = sf->unlock();	// TRUE
+	b = sf->unlock();	// FALSE
+
+	std::vector<ref<frost::api::sync_object>> vec;
+	vec.push_back(sf.get());
+	vec.push_back(mx.get());
+
+	b = frost::api::sync_object::lock_all(vec.data()->get_array(), vec.size());		// TRUE
+	b = frost::api::sync_object::lock_all(vec.data()->get_array(), vec.size());		// TRUaE
+	b = frost::api::sync_object::try_lock_all(vec.data()->get_array(), vec.size());	// FALSE
+	i = frost::api::sync_object::lock_one(vec.data()->get_array(), vec.size());		// 1
+	i = frost::api::sync_object::try_lock_one(vec.data()->get_array(), vec.size());	// 1
+}
