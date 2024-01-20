6b844425585d14321f97eb53a9563659efd2b92c
djordjermus <djordje.rmus2000@google.com>
2024-01-20 20:38:51 +0100

    Move Key class to separate file

---
+++ Frost.Net/Input/Key.cs
@@ -0,0 +1,30 @@
+﻿using Frost.Net.Interoperability;
+using Frost.Net.Utilities;
+
+namespace Frost.Net.Input;
+
+public static class Key
+{
+	public static string Translate(Keycode keycode, bool normalizeCase = true)
+	{
+		const int size = 16;
+		Span<char> chars = stackalloc char[size];
+		unsafe
+		{
+			fixed (char* output = chars)
+			{
+				var length = FrostApi.Keycode.ToString((byte)keycode, new IntPtr(output), size, normalizeCase);
+				return new string(chars.Slice(0, (int)length));
+			}
+		}
+	}
+
+	public static string GetName(Keycode keycode)
+	{
+		unsafe
+		{
+			var ptr = FrostApi.Keycode.GetName((byte)keycode);
+			return Unmanaged.StringFromUnmanagedWstr(ptr);
+		}
+	}
+}
--- Frost.Net/Input/Keycode.cs
+++ Frost.Net/Input/Keycode.cs
@@ -1,35 +1,4 @@
-﻿using Frost.Net.Interoperability;
-using Frost.Net.Utilities;
-
-namespace Frost.Net.Input;
-
-public static class Key
-{
-	public static string Translate(Keycode keycode, bool normalizeCase = true)
-	{
-		const int size = 16;
-		Span<char> chars = stackalloc char[size];
-		unsafe
-		{
-			fixed (char* output = chars)
-			{
-				var length = FrostApi.Keycode.ToString((byte)keycode, new IntPtr(output), size, normalizeCase);
-				return new string(chars.Slice(0, (int)length));
-			}
-		}
-	}
-
-	public static string GetName(Keycode keycode)
-	{
-		unsafe
-		{
-			var ptr = FrostApi.Keycode.GetName((byte)keycode);
-			return Unmanaged.StringFromUnmanagedWstr(ptr);
-		}
-	}
-}
-
-public enum Keycode : byte
+﻿public enum Keycode : byte
 {
 	Nullkey				= 0x00,
 
